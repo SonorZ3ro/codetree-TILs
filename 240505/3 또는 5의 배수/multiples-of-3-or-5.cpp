c76006274da60ad053d3248cc7202d56286e40c1
Park YeJun <151060766+SonorZ3ro@users.noreply.github.com>
2024-05-05 17:26:33 +0900

    Title: 3 또는 5의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240505/3 또는 5의 배수/multiples-of-3-or-5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    cin >> a;
+
+    a % 3 == 0 ? cout << "YES" << endl : cout << "NO" << endl;
+    a % 5 == 0 ? cout << "YES" << endl : cout << "NO" << endl;
+    return 0;
+}
