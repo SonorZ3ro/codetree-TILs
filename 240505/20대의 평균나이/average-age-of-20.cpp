2aae7173d7a38bf953c00addfba36c6002f89e1f
Park YeJun <151060766+SonorZ3ro@users.noreply.github.com>
2024-05-05 23:48:32 +0900

    Title: 20대의 평균나이, Time: 1078ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240505/20대의 평균나이/average-age-of-20.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int sum = 0;
+    int count = 0;
+    while(true){
+        int age;
+        cin >> age;
+        if(age >= 30) break;
+        sum += age;
+        count++;
+    }
+    double average = (double)sum / (double)count;
+
+    cout << fixed;
+    cout.precision(2);
+    cout << average;
+    return 0;
+}
