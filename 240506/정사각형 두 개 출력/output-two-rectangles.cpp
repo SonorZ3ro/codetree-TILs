83994750a93ed28d42d3131e64c991ea75202571
Park YeJun <151060766+SonorZ3ro@users.noreply.github.com>
2024-05-06 00:17:29 +0900

    Title: 정사각형 두 개 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240506/정사각형 두 개 출력/output-two-rectangles.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= 2; i++){
+        for(int j = 1; j <= n; j++){
+            for(int k = 1; k <= n; k++){
+                cout << "*";
+            }
+            cout << endl;
+        }
+        cout << '\n';
+    }
+    return 0;
+}
