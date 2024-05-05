c0b79d9e672ee754044146747b0a9127628c6893
Park YeJun <151060766+SonorZ3ro@users.noreply.github.com>
2024-05-06 04:53:29 +0900

    Title: 별표 출력하기 3, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240506/별표 출력하기 3/print-star-3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j < i; j++){
+            cout << "  ";
+        }
+        for(int j = 2*n -1; j >= 2*i -1; j--){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
