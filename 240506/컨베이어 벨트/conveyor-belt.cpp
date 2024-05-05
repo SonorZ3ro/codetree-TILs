543c20f62f1203b1681f288ff44ec0bc06d9a466
Park YeJun <151060766+SonorZ3ro@users.noreply.github.com>
2024-05-06 02:19:39 +0900

    Title: 컨베이어 벨트, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240506/컨베이어 벨트/conveyor-belt.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n, t;
+    cin >> n >> t;
+
+    //input array
+    int arr1[n];
+    int arr2[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr1[i];
+    }
+    for(int i = 0; i < n; i++){
+        cin >> arr2[i];
+    }
+
+    for(int cnt = 1; cnt <= t; cnt++){
+        int temp1 = arr1[n-1];
+        int temp2 = arr2[n-1];
+        for(int i = n-2; i >= 0; i--){
+            arr1[i+1] = arr1[i];
+        }
+        for(int i = n-2; i >= 0; i--){
+            arr2[i+1] = arr2[i];
+        }
+        arr2[0] = temp1;
+        arr1[0] = temp2;
+    }
+
+    for(int i = 0; i < n; i++){
+        cout << arr1[i] << " ";
+    }
+    cout << endl;
+    for(int i = 0; i < n; i++){
+        cout << arr2[i] << " ";
+    }
+    return 0;
+}
