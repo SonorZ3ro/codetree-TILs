f8ac2054e445ff37db5bb47ee477dfe7b25f6b19
Park YeJun <151060766+SonorZ3ro@users.noreply.github.com>
2024-05-06 00:05:31 +0900

    Title: 조건에 따른 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240506/조건에 따른 연산/operatino-by-rule.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    int cnt = 0;
+    while(true){
+        if(n >= 1000) break;
+        if(n % 2 == 0){
+            n *= 3;
+            n++;
+            cnt++;
+        }
+        else{
+            n *= 2;
+            n += 2;
+            cnt++;
+        }
+    }
+    cout << cnt;
+    return 0;
+}
