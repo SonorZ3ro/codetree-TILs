8601e78f9e8abaf7e21a3aa15b98dad47f363f51
Park YeJun <151060766+SonorZ3ro@users.noreply.github.com>
2024-05-05 17:27:44 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240505/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c, d, e;
+    cin >> a;
+    cin >> b >> c >> d >> e;
+    a > b ? cout << 1 << endl : cout << 0 << endl;
+    a > c ? cout << 1 << endl : cout << 0 << endl;
+    a > d ? cout << 1 << endl : cout << 0 << endl;
+    a > e ? cout << 1 << endl : cout << 0 << endl;
+    return 0;
+}
